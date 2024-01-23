ae555a4f59f18a072a363296b6b9b1cd3e2abe9d
Matt Straathof <matt.straat@gmail.com>
2024-01-23 14:03:26 -0800

    chore: allow for deploying prod/preprod inside of pipelines

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,19 +1,29 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import {MomentoVectorIndexChatDemoStack} from '../lib/mvi-chat-demo-stack';
+import { MomentoVectorIndexChatDemoStack } from '../lib/mvi-chat-demo-stack';
 
-// read from environment variable or panic
-const chatDomain: string | undefined = process.env.MVI_CHAT_DOMAIN;
-if (chatDomain === undefined) {
-  throw new Error('MVI_CHAT_DOMAIN environment variable must be set');
-}
+const isDevDeploy = process.env.IS_DEV_DEPLOY!!;
+const isProd = process.env.DEPLOY_ORG === 'prod';
 
+let chatDomain: string | undefined = "";
 let chatSubdomain: string | undefined = process.env.MVI_CHAT_SUBDOMAIN;
-if (chatSubdomain === undefined) {
-  console.log(
-    'MVI_CHAT_SUBDOMAIN environment variable not set, using "mo-chat"'
-  );
-  chatSubdomain = 'mo-chat';
+if (isDevDeploy) {
+  chatDomain = process.env.MVI_CHAT_DOMAIN;
+  if (chatDomain === undefined) {
+    throw new Error('MVI_CHAT_DOMAIN environment variable must be set');
+  }
+  if (chatSubdomain === undefined) {
+    console.log(
+      'MVI_CHAT_SUBDOMAIN environment variable not set, using "mo-chat"'
+    );
+    chatSubdomain = 'mo-chat';
+  }
+} else if (isProd) {
+  chatDomain = "mochat.momentohq.com"
+  chatSubdomain = "web"
+} else {
+  chatDomain = "mochat-preprod.momentohq.com"
+  chatSubdomain = "web"
 }
 
 const app = new cdk.App();

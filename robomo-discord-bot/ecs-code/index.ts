8a1500ae051c6aef231fc7ee1eb36473d2e928b2
anitarua <anita@momentohq.com>
2024-01-24 11:39:11 -0800

    chore: clean up some log statements

--- robomo-discord-bot/ecs-code/index.ts
+++ robomo-discord-bot/ecs-code/index.ts
@@ -1,70 +1,74 @@
-import { WebClient } from '@slack/web-api';
-import { Client, GatewayIntentBits } from 'discord.js';
+import {WebClient} from '@slack/web-api';
+import {Client, GatewayIntentBits} from 'discord.js';
 import {GetSecretValueCommand, SecretsManagerClient} from '@aws-sdk/client-secrets-manager';
 
 const _secretsClient = new SecretsManagerClient({});
 const _cachedSecrets = new Map<string, string>();
 
 async function main() {
-  console.log("In main!");
-
-  const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages] });
-
+  const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages],
+  });
   const web = await getSlackClient();
-
-  console.log("Initialized clients");
-  
-  let botId = "";
-  const slackChannelId = "C06F6APVB6H";
+  let botId = '';
+  const slackChannelId = 'C06F6APVB6H';
 
   client.once('ready', readyClient => {
     console.log(`Logged in as ${readyClient.user.tag}!`);
     botId = readyClient.user.id;
   });
 
-  client.on('messageCreate', async (message) => {
-    // Respond to any message that mentions the bot
+  client.on('messageCreate', async message => {
+    // Respond to any message that mentions the bot from any channel
     if (message.mentions.users.has(botId) && !message.author.bot) {
-      // ping robo mo api endpoint to search docs and get response
       let roboMoAnswered = false;
       for (let i = 0; i < 3; i++) {
         try {
-          const fetchResponse = await fetch("https://robomo-ls.mochat.momentohq.com/rag-momento-vector-index/invoke", {
-            method: "POST",
+          const fetchResponse = await fetch('https://robomo-ls.mochat.momentohq.com/rag-momento-vector-index/invoke', {
+            method: 'POST',
             headers: {
-              "Content-Type": "application/json",
+              'Content-Type': 'application/json',
             },
             body: JSON.stringify({input: message.cleanContent.replace('@RoboMo', '')}),
           });
           const roboMoAnswer = await fetchResponse.json();
-          console.log("Answer?", roboMoAnswer);
           message.reply(roboMoAnswer['output']);
           roboMoAnswered = true;
           break;
         } catch (error) {
-          console.log("Error fetching answer from RoboMo:", error);
+          console.log('Error fetching answer from RoboMo:', error);
           await sleep(1000);
         }
       }
       if (!roboMoAnswered) {
-        message.reply("Sorry, I'm having trouble answering your question right now. Please try again later or ask in the support channel to request help from Momento staff.");
+        message.reply(
+          "Sorry, I'm having trouble answering your question right now. Please try again later or ask in the support channel to request help from Momento staff."
+        );
       }
     }
 
     // Cross-post messages in the #support channel to a Slack channel
-    let supportChannel = message.guild?.channels.cache.find(channel => channel.name === "support");
-    if (message.channel.id === supportChannel?.id  && !message.author.bot) {
-      console.log("Support channel message: " + message.cleanContent);
-
-      // make sure message is the first from a user in the last 5 minutes
-      let lastElevenMessages = await message.channel.messages.fetch({ limit: 11 });
-      const fiveMinutes = 5 * 60 * 1000;
+    const supportChannel = message.guild?.channels.cache.find(channel => channel.name === 'support');
+    if (message.channel.id === supportChannel?.id && !message.author.bot) {
+      console.log('Support channel message: ' + message.cleanContent);
 
       // get the id of the newest message in the last 11 messages because it's the one that was just posted
-      let newestMessageId = lastElevenMessages.reduce((newest, obj) => newest === null ? obj : obj.createdTimestamp > newest.createdTimestamp ? obj : newest).id;
+      const lastElevenMessages = await message.channel.messages.fetch({limit: 11});
+      const fiveMinutes = 5 * 60 * 1000;
+      const newestMessageId = lastElevenMessages.reduce((newest, obj) =>
+        newest === null ? obj : obj.createdTimestamp > newest.createdTimestamp ? obj : newest
+      ).id;
 
-      if (lastElevenMessages.some(m => m.id !== newestMessageId && m.author.id === message.author.id && Date.now() - m.createdTimestamp < fiveMinutes)) {
-        console.log("Not posting message to Slack because user has posted in the last 5 minutes");
+      // make sure message is the first from a user in the last 5 minutes (don't want to spam our Slack channel)
+      if (
+        lastElevenMessages.some(
+          m =>
+            m.id !== newestMessageId &&
+            m.author.id === message.author.id &&
+            Date.now() - m.createdTimestamp < fiveMinutes
+        )
+      ) {
+        console.log('Not posting message to Slack because user has posted in the last 5 minutes');
       } else {
         // retry posting to slack 3 times before logging failure
         for (let i = 0; i < 3; i++) {
@@ -84,7 +88,7 @@ async function main() {
     }
   });
 
-  const discordToken = await getSecret("DiscordBotToken");
+  const discordToken = await getSecret('DiscordBotToken');
   client.login(discordToken);
 }
 
@@ -93,9 +97,8 @@ function sleep(ms: number) {
 }
 
 async function getSlackClient(): Promise<WebClient> {
-  const token = await getSecret("SlackToken");
+  const token = await getSecret('SlackToken');
   const client = new WebClient(token);
-  console.log("Created Slack client");
   return client;
 }
 
@@ -112,6 +115,5 @@ async function getSecret(secretName: string): Promise<string> {
 }
 
 main()
-.then(() => console.log("Main complete!"))
-.catch((error) => console.error("Error in main:", error));
-
+  .then(() => console.log('Main complete!'))
+  .catch(error => console.error('Error in main:', error));
